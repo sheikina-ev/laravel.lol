26d4d8edf0a7ed01187e4577d0d034ae6cf102f0
sheikina-ev <liza.sheykina.2005@mail.ru>
2023-11-11 16:13:51 +0700

    Изучили маршрутизацию

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,79 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+/* Создание маршрута /test с выводом сообщения "Тестовая страница!" */
+Route::get('/test', function (){
+   return "Тестовая страница!";
+});
+Route::get('/test/1', function (){
+    return "Тестовая страница 1!";
+});
+Route::get('/test/2', function (){
+    return "Тестовая страница 2!";
+});
+
+/* Параметры в маршруте */
+Route::get('/post/{id}', function ($id){
+    return "Пост " . $id;
+});
+Route::get('/user/{name}', function ($name){
+    return "Привет,  " . $name;
+});
+
+/* Несколько параметров в маршруте */
+Route::get('/post/{cardId}/{postId}', function ($cardId, $postId){
+    return $cardId . " - " . $postId;
+});
+Route::get('/user/{surname}/{name}', function ($surname, $name){
+    return "Привет, " . $surname . " " . $name . "!";
+});
+
+/* Необязательные параметры */
+Route::get('/posts/page/{page?}', function ($page = 1){
+    return "Номер страницы: " . $page;
+});
+
+/* :city  = название переменной */
+Route::get('/city/{city?}', function ($city = 'Томск'){
+    return "Город: " . $city;
+});
+
+/* Ограничение параметров */
+Route::get('/users/{age}', function ($age){
+    return "Возраст пользователя: " . $age;
+})->where('age', '[0-9]+');
+
+Route::get('/govsing/{sign}/{id}', function ($sign, $id){
+    return "Номер: " . $sign . ". Регион: " . $id . ".";
+})->where('sign', '[a-z]+')->where('id', '[0-9]+');
+
+/* whereAlpha - ограничение только на буквы
+   whereNumber - ограничение только на цифры
+   whereAlphaNumeric - ограничение на буквы и цифры
+    ограничение на id сделано глобально на све маршруты в файле App\Providers\RouteServiceProvider.php */
+
+Route::get('/govsing2/{sign}/{id}', function ($sign, $id){
+    return "Номер: " . $sign . ". Регион: " . $id . ".";
+})->whereAlpha('sign');
+
+/* Разрешение конфликтов в маршрутах
+   сначало указываем частные случаи, потом общие */
+Route::get('/test2/all', function (){
+    return "Все тесты";
+});
+Route::get('/test2/{n}', function ($n){
+    return "Тест - " . $n;
+});
+
+/* Группировка маршрутов */
+Route::prefix('/test3')->group( function (){
+
+    Route::get('/all', function (){
+        return "Все тесты";
+    });
+
+    Route::get('/{n}', function ($n){
+        return "Тест - " . $n;
+    });
+});
+
